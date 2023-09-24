209f1151192d7019bb58d28815535c3eda63bfa1
Miguel Ángel Sarmiento Levy <112961729+miguelASL@users.noreply.github.com>
2023-09-25 00:54:21 +0200

    Update qr.py
    Cambio en el input

--- qr.py
+++ qr.py
@@ -1,7 +1,7 @@
 import qrcode
 
 # Create qr code instance
-texto = input("Digite o texto para generar o QR Code: ")
+texto = input("Diga el texto que quiera para generar el QR Code: ")
 
 qr = qrcode.QRCode(version=1, box_size=10, border=5)  
 
